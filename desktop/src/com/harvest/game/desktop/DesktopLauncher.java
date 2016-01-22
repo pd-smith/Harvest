9c190f104ceef0ea183bdfb045825c87c44c4faa
unknown <psmith4@buffalo.edu>
2016-01-22 17:22:51 -0500

    Initial push

---
+++ desktop/src/com/harvest/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.harvest.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.harvest.game.GameDriver;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameDriver(), config);
+	}
+}
